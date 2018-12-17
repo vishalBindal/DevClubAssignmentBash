30c88103c812e4cc80af84c2c99680c116e59502
vishalBindal <vishalbindal.06@gmail.com>
2018-12-17 20:13:52 +0530

    First script added

---
+++ countFiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ ($# -ne 1) && ($# -ne 2) ]]
+then
+exit -1
+
+elif [ ! -d $1 ]
+then
+exit -1
+
+elif [[ ("$2" != "."*) && ("$2" != "") ]]
+then
+exit -1
+fi
+
+find $1 -maxdepth 1 -type f -name "*$2" | wc -l
+
+exit 0
